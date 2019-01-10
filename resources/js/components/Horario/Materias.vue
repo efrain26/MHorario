2b8d59de3369d251c51c5ac4f27a6c9cdea93248
Cesar010 <cesarfuentes010@gmail.com>
2019-01-10 15:03:39 -0600

    Update Materias.vue

--- resources/js/components/Horario/Materias.vue
+++ resources/js/components/Horario/Materias.vue
@@ -1,41 +1,46 @@
 <template>
-    <div class="container">
-        <div class="row">
+    <div>
+        <div class="text">
+            <h1>Seleccionar materia</h1>
+        </div>
+        <div class="">
+            <div class="">
+                <div class="col-md-10">
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th scope="col">Clave</th>
+                            <th scope="col">Materia</th>
+                            <th scope="col">Semestre</th>
+                            <th scope="col">Lunes</th>
+                            <th scope="col">Martes</th>
+                            <th scope="col">Miercoles</th>
+                            <th scope="col">Jueves</th>
+                            <th scope="col">Viernes</th>
+                            <th scope="col">Seleccionar</th>
 
-            <div class="text">
-                <h1>Seleccionar materia a impartir</h1>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">5FP1</th>
+                            <td>Calculo Integral</td>
+                            <td>5</td>
+                            <td>07:50-08:50</td>
+                            <td>07:50-08:50</td>
+                            <td>07:50-08:50</td>
+                            <td>07:50-08:50</td>
+                            <td>07:50-08:50</td>
+                            <td>  <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+
             </div>
-            <table class="table table-hover">
-                <thead>
-                <tr>
-                    <th scope="col">Clave</th>
-                    <th scope="col">Materia</th>
-                    <th scope="col">Semestre</th>
-                    <th scope="col">Lunes</th>
-                    <th scope="col">Martes</th>
-                    <th scope="col">Miercoles</th>
-                    <th scope="col">Jueves</th>
-                    <th scope="col">Viernes</th>
-                    <th scope="col">Seleccionar</th>
-
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <th scope="row">5FP1</th>
-                    <td>Calculo Integral</td>
-                    <td>5</td>
-                    <td>07:50-08:50</td>
-                    <td>07:50-08:50</td>
-                    <td>07:50-08:50</td>
-                    <td>07:50-08:50</td>
-                    <td>07:50-08:50</td>
-                    <td>  <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-            <div class="container">
+        </div>
+        <div class="container">
                 <div class="row">
                     <div class="col-md-10 col-lg-10"></div>
                     <div class="col-md-1 col-lg-1"></div>
@@ -44,8 +49,6 @@
                     </div>
                 </div>
             </div>
-
-        </div>
     </div>
 </template>
 
@@ -60,11 +63,15 @@
         text-align: center;
     }
     h1{
-        margin-left: 10rem;
-        font-size: 3.25rem;
+        font-size: 2.3rem;
+        margin-top: 1rem;
+        text-align: center;
     }
     .text
     {
-        margin-bottom: 35px;
+        margin-bottom: 15px;
+    }
+    .btn{
+        margin-left: 1.8rem;
     }
 </style>
